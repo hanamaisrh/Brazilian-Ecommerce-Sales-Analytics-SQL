fd2e284657c59318cb12619426e9638e8d02e47b
Hana Maisarah Meor Syaifudin <hmsrhna2004@gmail.com>
2026-07-28 14:55:34 +0800

    Update dataset paths in SQL queries

--- queries.sql
+++ queries.sql
@@ -11,7 +11,7 @@
 SELECT 
     COUNT(DISTINCT order_id) AS total_orders,
     ROUND(SUM(price), 2) AS total_revenue
-FROM '/kaggle/input/brazilian-ecommerce/olist_order_items_dataset.csv';
+FROM '/kaggle/input/datasets/organizations/olistbr/brazilian-ecommerce/olist_order_items_dataset.csv';
 
 
 -- ------------------------------------------------------------
@@ -20,8 +20,8 @@ FROM '/kaggle/input/brazilian-ecommerce/olist_order_items_dataset.csv';
 SELECT 
     p.product_category_name AS category_name,
     ROUND(SUM(i.price), 2) AS total_revenue
-FROM '/kaggle/input/brazilian-ecommerce/olist_order_items_dataset.csv' i
-JOIN '/kaggle/input/brazilian-ecommerce/olist_products_dataset.csv' p 
+FROM '/kaggle/input/datasets/organizations/olistbr/brazilian-ecommerce/olist_order_items_dataset.csv' i
+JOIN '/kaggle/input/datasets/organizations/olistbr/brazilian-ecommerce/olist_products_dataset.csv' p 
   ON i.product_id = p.product_id
 WHERE p.product_category_name IS NOT NULL
 GROUP BY category_name
@@ -36,7 +36,7 @@ SELECT
     payment_type,
     COUNT(DISTINCT order_id) AS total_orders,
     ROUND(AVG(payment_value), 2) AS avg_order_value
-FROM '/kaggle/input/brazilian-ecommerce/olist_order_payments_dataset.csv'
+FROM '/kaggle/input/datasets/organizations/olistbr/brazilian-ecommerce/olist_order_payments_dataset.csv'
 GROUP BY payment_type
 ORDER BY total_orders DESC;
 
@@ -48,8 +48,8 @@ WITH monthly_sales AS (
     SELECT 
         DATE_TRUNC('month', CAST(o.order_purchase_timestamp AS TIMESTAMP)) AS month,
         ROUND(SUM(i.price), 2) AS total_revenue
-    FROM '/kaggle/input/brazilian-ecommerce/olist_orders_dataset.csv' o
-    JOIN '/kaggle/input/brazilian-ecommerce/olist_order_items_dataset.csv' i 
+    FROM '/kaggle/input/datasets/organizations/olistbr/brazilian-ecommerce/olist_orders_dataset.csv' o
+    JOIN '/kaggle/input/datasets/organizations/olistbr/brazilian-ecommerce/olist_order_items_dataset.csv' i 
       ON o.order_id = i.order_id
     WHERE o.order_status = 'delivered'
     GROUP BY month
@@ -69,5 +69,5 @@ ORDER BY month;
 SELECT 
     ROUND(AVG(DATE_DIFF('day', CAST(order_purchase_timestamp AS TIMESTAMP), CAST(order_delivered_customer_date AS TIMESTAMP))), 1) AS avg_delivery_days,
     ROUND(COUNT(CASE WHEN CAST(order_delivered_customer_date AS TIMESTAMP) > CAST(order_estimated_delivery_date AS TIMESTAMP) THEN 1 END) * 100.0 / COUNT(order_id), 2) AS late_delivery_percentage
-FROM '/kaggle/input/brazilian-ecommerce/olist_orders_dataset.csv'
+FROM '/kaggle/input/datasets/organizations/olistbr/brazilian-ecommerce/olist_orders_dataset.csv'
 WHERE order_status = 'delivered';
